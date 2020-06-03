0fbbd9cf8d8c168fbb8b395b041e1a6dbce48876
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-06-02 20:47:24 -0600

    :crystal_ball: completed API fetch data

--- src/apiFetch.js
+++ src/apiFetch.js
@@ -2,4 +2,77 @@ class ApiFetch{
   constructor() {
     this.rootURL = "https://fe-apps.herokuapp.com/api/v1/travel-tracker/data"
   }
+  getTravelersData() {
+    let url = `${this.rootUrl}/travelers/travelers`
+    return fetch(url).then(response => response.json())
+  }
+
+  getSingleTravelerData(id) {
+    let url = `${this.rootUrl}/travelers/travelers/${id}`
+    return fetch(url).then(response => response.json())
+  }
+
+  getAllTrips() {
+    let url = `${this.rootUrl}/trips/trips`
+    return fetch(url).then(response => response.json())
+  }
+
+  getAllDestinations() {
+    let url = `${this.rootUrl}/destinations/destinations`
+    return fetch(url).then(response => response.json())
+  }
+
+  postNewTrip(TOBEDETERMINEDOBJECT) {
+    let url = `${this.rootUrl}/trips/trips`;
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(TOBEDETERMINEDOBJECT),
+      // {id: <number>, 
+      // userID: <number>, 
+      // destinationID: <number>, 
+      // travelers: <number>, 
+      // date: <string 'YYYY/MM/DD'>, 
+      // duration: <number>, 
+      // status: <string 'approved' or 'pending'>, 
+      // suggestedActivities: <array of strings>}
+    })
+      .then(response => console.log(response.json()))
+      .catch(err => console.log(err.message));
+  }
+
+  modifySingleTrip(TOBEDETERMINEDOBJECT) {
+    let url = `${this.rootUrl}/trips/updateTrip`;
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(TOBEDETERMINEDOBJECT),
+      // {id: <number>, 
+      // status:<String of 'approved' or 'pending', 
+      // suggestedActivities: <Array of strings>} 
+      // Only a status or a suggestedActivities property is required for a successful request
+    })
+      .then(response => console.log(response.json()))
+      .catch(err => console.log(err.message));
+  }
+
+  deleteSingleTrip(TOBEDETERMINEDOBJECT) {
+    let url = `${this.rootUrl}/trips/trips`;
+    return fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(TOBEDETERMINEDOBJECT),
+      // {id: <number>} 
+    })
+      .then(response => console.log(response.json()))
+      .catch(err => console.log(err.message));
+  }
 }
+
+export default ApiFetch;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
-// An example of how you tell webpack to use a CSS (SCSS) file
+// Imports
 import './css/base.scss';
 import "./css/styles.scss";
+import ApiFetch from './apiFetch';
+
+
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/loginPageTravel.jpg'
 
