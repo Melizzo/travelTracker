c99da35de4a81a5b5fa8219530bcf12b7e194be6
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-06-07 21:04:01 -0600

    :cbar_chart: completed test for destinations found for travelers trips

--- test/traveler-test.js
+++ test/traveler-test.js
@@ -36,13 +36,13 @@ let traveler;
     expect(traveler.destinationsData).to.equal('Error, data for traveler\'s data cannot be found.')
   });
 
-  it('should have a method that determines whether data passed in is an object', function() {
+  it('should have a method that determines whether data passed in is the correct data', function() {
     traveler = new Traveler('horse')
     expect(traveler.checkIfDataIsAnObject('horse')).to.equal('Error, data for the traveler cannot be found.')
     expect(traveler.travelerData).to.equal('Error, data for the traveler cannot be found.')
 
     const traveler2 = new Traveler(travelerData, tripsData, destinationData)
-    expect(traveler2.checkIfDataIsArray(travelerData)).to.equal(travelerData)
+    expect(traveler2.checkIfDataIsAnObject(travelerData)).to.equal(travelerData)
     expect(traveler2.checkIfDataIsArray(tripsData)).to.equal(tripsData)
     expect(traveler2.checkIfDataIsArray(destinationData)).to.equal(destinationData)
     expect(traveler2.travelerData).to.deep.equal(travelerData)
@@ -89,6 +89,25 @@ let traveler;
     ])
   })
 
+  it('should be able to find the destinations that match with a traveler\'s trips', function() {
+    expect(traveler.findDestinationsOfTravelersTrips()).to.deep.equal([{
+      id: 4,
+      destination: 'Cartagena, Colombia',
+      estimatedLodgingCostPerDay: 65,
+      estimatedFlightCostPerPerson: 350,
+      image: 'https://images.unsplash.com/photo-1558029697-a7ed1a4b94c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+      alt: 'boats at a dock during the day time'
+    },
+    {
+      id: 5,
+      destination: 'Madrid, Spain',
+      estimatedLodgingCostPerDay: 150,
+      estimatedFlightCostPerPerson: 650,
+      image: 'https://images.unsplash.com/photo-1543785734-4b6e564642f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+      alt: 'city with clear skys and a road in the day time'
+    }])
+  })
+
   it('should be able to total the cost of all the travel\'s lodging per trip', function() {
     expect(traveler.calculateTotalLodgingCostPerTripThisYear(traveler.findTravelerTrips())).to.equal(1850)
   })
@@ -98,12 +117,11 @@ let traveler;
   })
 
   it('should be able to total the cost of all the traveler\'s trip', function() {
-    expect(traveler.calculateTotalCostOfTrips(1850, 1350)).to.equal(3200)
+    expect(traveler.calculateTotalCostOfTrips(traveler.calculateTotalFlightCostPerTripThisYear(), traveler.calculateTotalLodgingCostPerTripThisYear())).to.equal(3200)
   })
 
   it('should be able to show the 10% Travel Agency fees', function() {
     expect(traveler.calculateTravelAgency10PercentFee(3200)).to.equal(320)
   })
-
 
 })
